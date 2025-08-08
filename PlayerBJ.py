6e7fc076103014c2eadb44ae4b083b71bc4dcc37
Adrian-E-V <villanuevaeadrian@gmail.com>
2025-08-07 23:55:03 -0400

    Finished the game! Still some more features and testing to add/do, but the game is working and playable!

--- PlayerBJ.py
+++ PlayerBJ.py
@@ -1,5 +1,7 @@
 import dealerBJ as dealer
 import bj_util
+import math
+import time
 
 #Ideas for additional improvement:
 # - Double and Split functionality
@@ -12,6 +14,8 @@ cash = 1000
 def placeBets():
     global cash
     bet = input("Place your bets (enter number or 'ALL IN'): ")
+    if (bet == "QUIT"):
+        quit()
     if (bet == "ALL IN"):
         return cash  
     else:
@@ -37,6 +41,48 @@ def placeBets():
             else:
                 return bet
         return placeBets()
+
+def countTotal(hand):
+    total = 0
+    aces = False
+    count = 0
+    for card in hand:
+        if (card.getRank() == "Ace"):
+            aces = True
+            count += 1
+        else:
+            try:
+                total += card.getRank()
+            except TypeError:
+                total += 10
+    if aces:
+        #for EACH Ace in the player's hand, we must add 1
+        #In addition, if adding 10 more (total of 11) doesn't put the player over 21, then add 10.
+        #Do these for each ace in the player's hand (the count)
+        i = 0
+        while i < count:
+            total += 1
+            if ((total + 10) <= 21):
+                total += 10
+            i += 1
+    return total
+
+def win(bet: int, blackJack=False):#call this when the player wins
+    global cash
+    if (blackJack):
+        cash += math.ceil(bet/2)
+        print(f"Blackjack! You now have ${cash} \n")
+    else:
+        cash += bet
+        print(f"You win! You now have ${cash} \n")
+
+def lose(bet: int):#call when player loses
+    global cash
+    cash -= bet
+    if (cash <= 0):
+        return
+    else:
+        print(f"You lose! You now have ${cash} \n")
     
 def round():
     global cash
@@ -54,15 +100,23 @@ def round():
 
     print(f"Dealer is showing a {dlrHand[0]}")
     print(f"You have: {plrHand[0]}, {plrHand[1]}")
-    action = ""
+    plrTotal = countTotal(plrHand)
+    print(f"Your total: {plrTotal}")
+    if (plrTotal == 21):
+        win(bet, True)
+        return
     print()
+    
+
+    
     #figure out how to do aces
     #Include player total (if ace do hard / soft)
-    
+    action = ""
     while(action != "STAND"):
         action = input("Do you HIT or STAND? ")
         if (action == "HIT"):
             plrHand.append(dealer.dealCard())
+            plrTotal = countTotal(plrHand)
             print("You have:", end=" ")
             i = 0
             while (i < len(plrHand)):
@@ -71,7 +125,12 @@ def round():
                 else:
                     print(plrHand[i], end=", ")
                 i += 1
+            print(f"Your total: {plrTotal}")
+            if (plrTotal > 21):
+                lose(bet)
+                return
             print()
+
         elif (action == "STAND"):
             print()
         else:
@@ -79,8 +138,41 @@ def round():
             print("Invalid action, try again.")
      
     #After the part where the player gambles, this is where we calculate whether they have won or lost
-    #I need to see if they lose from hitting. This is a check that needs to be performed earlier, probably when a player HITs
+    dlrTotal = countTotal(dlrHand)
+    print(f"Dealer has: {dlrHand[0]}, {dlrHand[1]}")
+    time.sleep(0.5)
+    while (dlrTotal<17):
+        time.sleep(1.5)
+        print("Dealer draws...")
+        dlrHand.append(dealer.dealCard())
+        dlrTotal = countTotal(dlrHand)
+        time.sleep(1.5)
+        print(dlrHand[-1])
+        time.sleep(0.5)
+    print(f"Dealer's total: {dlrTotal}")
+    time.sleep(0.5)
+    
+    if (dlrTotal <= 21):
+        if (plrTotal > dlrTotal):
+            #player wins!
+            win(bet)
+            return
+        if (dlrTotal > plrTotal):
+            #player loses
+            lose(bet)
+            return
+        if (plrTotal == dlrTotal):
+            print(f"Push! You keep your money. You have ${cash}\n")
+            return
+    else:
+        win(bet)
+        return
     
 
-
-round()
+while (cash>0 and cash<2500):
+    round()
+    time.sleep(1.5)
+if (cash <= 0):
+    print("You lose! You have lost all your cash.")
+else:
+    print("You've beaten BlackJack!")
